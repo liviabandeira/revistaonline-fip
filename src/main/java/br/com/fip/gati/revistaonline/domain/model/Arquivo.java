200b42fe70e063caf0ef77172b6d0cfe92cdb765
liviabandeira <Livia@Livia-PC>
2014-02-25 09:28:21 -0300

    Tarefa de Upload de artigos

---
+++ src/main/java/br/com/fip/gati/revistaonline/domain/model/Arquivo.java
@@ -0,0 +1,40 @@
+package br.com.fip.gati.revistaonline.domain.model;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+public class Arquivo {
+	
+	private File pastaArquivo;
+	static int nome = 0;
+	
+	public Arquivo (ServletContext context){
+		
+		String caminhoArquivo = context.getRealPath("WEB-INF/artigo");
+		pastaArquivo = new File(caminhoArquivo);
+		pastaArquivo.mkdir();
+	}
+	
+	public void salva(UploadedFile arquivo){
+		
+		File destino = new File(pastaArquivo, String.valueOf(nome) + ".txt");
+		nome ++;
+		try {
+			IOUtils.copy(arquivo.getFile(), new FileOutputStream(destino));
+		} catch (IOException e ) {
+			throw new RuntimeException("Erro ao copiar arquivo", e);
+		}
+		
+	}
+
+}
+
