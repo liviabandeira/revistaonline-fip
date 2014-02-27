6e486a1676a4edf222e89a590aa15f0ba2910fc4
liviabandeira <Livia@Livia-PC>
2014-02-27 15:36:59 -0300

    Conflitos resolvidos

--- src/main/java/br/com/fip/gati/revistaonline/resources/web/controllers/ArquivoController.java
+++ src/main/java/br/com/fip/gati/revistaonline/resources/web/controllers/ArquivoController.java
@@ -3,26 +3,31 @@ package br.com.fip.gati.revistaonline.resources.web.controllers;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.interceptor.multipart.UploadedFile;
-import br.com.fip.gati.revistaonline.dao.ArquivoDAO;
+import br.com.fip.gati.revistaonline.domain.model.Arquivo;
 
 @Resource
 public class ArquivoController {
-
-	private ArquivoDAO arquivo;
-
-	public ArquivoController(ArquivoDAO arq) {
-		this.arquivo = arq;
+	
+	private final Arquivo arquivo;
+	private final Result result;
+	
+	public ArquivoController (Result result, Arquivo arquivo ){
+		this.arquivo = arquivo;
+		this.result = result;
 	}
-
-	@Get("/upload")
-	public void upload() {
+	
+	@Get("/upload") 
+	public void formulario(){
 	}
-
-	@Post("/upload/file")
-	public void upload(UploadedFile file) {
-		this.arquivo.salva(file);
-
+	
+	@Post("/upload")
+	public void upload (final UploadedFile arquivo){
+		
+		this.arquivo.salva(arquivo);
+		result.redirectTo(LoginController.class).login();
 	}
 
 }
+
